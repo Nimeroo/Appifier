67772fdd52286b273d467371e446dd7dfcb3ff71
Nimeroo <j.giroux9@gmail.com>
2021-06-29 03:32:21 -0400

    Edit toggle added

--- client/src/components/GameComments/GameComments.jsx
+++ client/src/components/GameComments/GameComments.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GameComments.css";
 import UserInfo from "../UserInfo/UserInfo";
 
 const GameComments = (props) => {
+  const [disableStatus, setDisableStatus] = useState(true);
+  const [edit, setEdit] = useState("Edit");
+
   return (
     <div className="comments-list">
       {props.comments.map((comment) => {
         return (
           <div className="comment-div">
             <UserInfo user={props.user} />
-            <h4 className="comment-content">{comment.content}</h4>
+            <div>
+              <button
+                onClick={() => {
+                  setDisableStatus(!disableStatus);
+                  if (disableStatus == true) {
+                    setEdit("Submit");
+                  } else {
+                    setEdit("Edit");
+                  }
+                }}
+              >
+                {edit}
+              </button>
+            </div>
+            <textarea disabled={disableStatus} className="comment-content">
+              {comment.content}
+            </textarea>
           </div>
         );
       })}
